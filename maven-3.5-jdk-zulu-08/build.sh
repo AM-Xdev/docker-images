24fb46c022af243dc1314b3b12a0e7b5f153debb
B1GMAMMA\Bigmama <arne_meier@freenet.de>
2019-11-06 21:39:48 +0100

    maven no jdk zulu angepasst

--- maven-3.5-jdk-zulu-08/build.sh
+++ maven-3.5-jdk-zulu-08/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker build -t svenruppert/maven-3.5-jdk-zulu-08 .
-docker push svenruppert/maven-3.5-jdk-zulu-08:latest
+docker build -t amgamen/maven-3.5-jdk-zulu-08 .
+docker push amgamen/maven-3.5-jdk-zulu-08:latest
 
-docker tag svenruppert/maven-3.5-jdk-zulu-08:latest svenruppert/maven-3.5-jdk-zulu-08:1.8.1.8.172
-docker push svenruppert/maven-3.5-jdk-zulu-08:1.8.1.8.172
+docker tag amgamen/maven-3.5-jdk-zulu-08:latest amgamen/maven-3.5-jdk-zulu-08:1.8.1.8.172
+docker push amgamen/maven-3.5-jdk-zulu-08:1.8.1.8.172
 

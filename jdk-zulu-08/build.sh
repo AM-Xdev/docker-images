15c2a10b57a2adb6c7233c36a06cfdc6f6cabac6
AM-Xdev <40858279+AM-Xdev@users.noreply.github.com>
2020-03-18 13:47:57 +0100

    Update build.sh
    Update tags to 242

--- jdk-zulu-08/build.sh
+++ jdk-zulu-08/build.sh
@@ -2,6 +2,6 @@
 docker build -t amxdev/jdk-zulu-08 .
 docker push amxdev/jdk-zulu-08:latest
 
-docker tag amxdev/jdk-zulu-08:latest amxdev/jdk-zulu-08:1.8.172
-docker push amxdev/jdk-zulu-08:1.8.172
+docker tag amxdev/jdk-zulu-08:latest amxdev/jdk-zulu-08:1.8.242
+docker push amxdev/jdk-zulu-08:1.8.242
 
